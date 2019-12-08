909b1069f752d740daac681e3639a7f2640ad9a9
zhaoyuankui <zhaoyuankui@p1.com>
2019-12-08 19:43:23 +0800

    Add backtrace resolution for permute

--- permute/permute_test.go
+++ permute/permute_test.go
@@ -52,3 +52,51 @@ func permute(nums []int) [][]int {
 	}
 	return res
 }
+
+func Test_permute2(t *testing.T) {
+	assert.Equal(t, [][]int{}, permute2(alg.IntSlice()))
+	assert.Equal(t, [][]int{[]int{0}}, permute2(alg.IntSlice(0)))
+	assert.Equal(t, 6, len(permute2(alg.IntSlice(1, 2, 3))))
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{1, 2, 3})
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{1, 3, 2})
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{2, 3, 1})
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{2, 1, 3})
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{3, 1, 2})
+	assert.Contains(t, permute2(alg.IntSlice(1, 2, 3)), []int{3, 2, 1})
+}
+
+func Benchmark_permute2(b *testing.B) {
+	// Initializations here.
+	b.ResetTimer()
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i := 0; i < b.N; i++ {
+		permute2(nums)
+	}
+}
+
+func permute2(nums []int) [][]int {
+	res := [][]int{}
+	if len(nums) == 0 {
+		return res
+	}
+	flags := make(map[int]bool, len(nums))
+	v := make([]int, len(nums))
+	backtrace(nums, flags, &res, v, 0)
+	return res
+}
+
+func backtrace(nums []int, flags map[int]bool, res *[][]int, v []int, d int) {
+	if d == len(nums) {
+		*res = append(*res, append([]int{}, v...))
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		if flags[i] {
+			continue
+		}
+		flags[i] = true
+		v[d] = nums[i]
+		backtrace(nums, flags, res, v, d+1)
+		flags[i] = false
+	}
+}
